60c2dbcede3e77eae4773b4b3f61f45e52ef6b9f
EyalOlevson <eyal.olevson@gmail.com>
2018-11-05 13:13:34 +0200

    Almost Done. only reports implementation required.

---
+++ src/ExtentReports.java
@@ -0,0 +1,5 @@
+public class ExtentReports {
+    private static ExtentReports extent = new ExtentReports("C://1.html",false);
+    private static ExtentTest test = extent.startTest("My test","Sanity website test");
+
+    }
