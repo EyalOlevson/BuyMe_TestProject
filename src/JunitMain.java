aa89521922cb3290c2f4bd9ccaf1cf9eadefffe5
EyalOlevson <eyal.olevson@gmail.com>
2018-11-05 12:15:38 +0200

    Almost Done. only reports implementation required.

--- src/JunitMain.java
+++ src/JunitMain.java
@@ -6,53 +6,33 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.ie.InternetExplorerDriver;
-import org.w3c.dom.Document;
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import java.io.File;
 import java.util.concurrent.TimeUnit;
 
 @FixMethodOrder (MethodSorters.NAME_ASCENDING)
 public class JunitMain {
     private static WebDriver driver;
-    @BeforeClass
 
-    public static void runBefore () throws Exception {
-        String browserType = getData ("Browser");
-        if(browserType.equals("Chrome")){
+    @BeforeClass
+    public static void runBefore() throws Exception {
+        String browserType = ReadXML.getData("Browser");
+        if (browserType.equals("Chrome")) {
             System.setProperty("webdriver.chrome.driver", "C:\\Eyal's Automation Projects\\chromedriver_win32\\chromedriver.exe");
             ChromeOptions options = new ChromeOptions();
             options.addArguments("start--incognito");
             driver = new ChromeDriver(options);
-        }else if(browserType.equals("IE")){
+        } else if (browserType.equals("IE")) {
             driver = new InternetExplorerDriver();
         }
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-        driver.get(getData("URL"));
+        driver.get(ReadXML.getData("URL"));
     }
-    public static String getData (String keyName) throws Exception{
-        File configXmlFile=new File("C:\\Users\\Michal\\Desktop\\Config2.xml");
-        DocumentBuilderFactory dbFactory=DocumentBuilderFactory.newInstance();
-        DocumentBuilder dBuilder=dbFactory.newDocumentBuilder();
-        Document doc=dBuilder.parse(configXmlFile);
-        doc.getDocumentElement().normalize();
-        return doc.getElementsByTagName(keyName).item(0).getTextContent();
-    }
-//        System.setProperty("webdriver.chrome.driver", "C:\\Eyal's Automation Projects\\chromedriver_win32\\chromedriver.exe");
-        //new NgWebDriver((JavascriptExecutor)driver).waitForAngularRequestsToFinish();
-
-//        static ChromeOptions options = new ChromeOptions();
-//        options.addArguments("-incognito");
-//        options.addArguments("start--incognito");
-//        driver = new ChromeDriver(options);
-//        driver.manage().window().maximize();
-//        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-//        driver.get("https://buyme.co.il");
-
-    @Test
+//    extent.addSystemInfo("Enviroment","Production");
+//    //log information into the report
+//    test.log(LogStatus.INFO,"Connection driver","Before test method")
+//}
+    @Test //Test registration
     public void myTest01(){
-
         HomeScreen.pressLogin(driver);
         RegistrationScreen.ClickRegister(driver);
         RegistrationScreen.EnterName(driver);
@@ -62,7 +42,7 @@ public class JunitMain {
         RegistrationScreen.RegConsent(driver);
         RegistrationScreen.SubmitRegistration(driver);
     }
-    @Test
+    @Test //Test searching and choosing a gift
     public void myTest02() throws InterruptedException {
         Thread.sleep(1000);
         HomeScreen.ChooseAmount(driver);
@@ -83,17 +63,24 @@ public class JunitMain {
         GiftScreen.EventType(driver);
         GiftScreen.CurrentEventID(driver);
     }
-    @Test
+    @Test //Test the Radio buttons for when to pay
     public void myTest03() throws InterruptedException {
         GiftScreen.When2PaySelector(driver);
         GiftScreen.Where2SendGift(driver);
     }
-    @Test
+    @Test //Test uploading image
         public void myTest04() throws InterruptedException {
             GiftScreen.UploadPic(driver);
         }
-    @Test
-    public void myTest05(){
-    GiftScreen.SubmitGiftDetails(driver);
-    }
-}
+    @Test //Test submit button
+    public void myTest05() {
+        GiftScreen.SubmitGiftDetails(driver);
+    }}
+
+//@AfterClass
+//public static void afterClass() {
+////    end test and save data into report file
+//    extent.endTesst(test);
+//    extent.flush();
+//    }
+//}
--- src/ReadXML.java
+++ src/ReadXML.java
@@ -1,50 +1,18 @@
-//import org.junit.BeforeClass;
-//import org.w3c.dom.Document;
-//import org.xml.sax.SAXException;
-//
-//import javax.xml.parsers.DocumentBuilder;
-//import javax.xml.parsers.DocumentBuilderFactory;
-//import javax.xml.parsers.ParserConfigurationException;
-//import java.io.File;
-//import java.io.IOException;
-//
-//@BeforeClass
-//public void ChooseBrowser throws Exception{
-//        String browserType = getData ("BrowserType");
-//        if(browserType.equals("Chrome")){
-//        options.addArguments("start--incognito");
-//        driver = new ChromeDriver(options);
-//        }else if(browserType.equals("IE")){
-//        driver = new InternetExplorerDriver();
-//        }
-//        driver.manage().window().maximize();
-//        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-//        driver.get("https://buyme.co.il");
-//        }
-//public static String getData (String keyName) throws Exception{
-//        File configXmlFile=new File("C:\\Users\\Michal\\Desktop\\Config2.xml");
-//        DocumentBuilderFactory dbFactory=DocumentBuilderFactory.newInstance();
-//        DocumentBuilder dBuilder=dbFactory.newDocumentBuilder();
-//        Document doc=dBuilder.parse(fXmlFile);
-//        doc.getDocumentElement().normalize();
-//        return doc.getElementsByTagName(keyName).item(0).getTextContent();
-//        }
-//
-////public class ReadXML {
-////    private static String getData(String keyName) throws ParserConfigurationException, IOException, SAXException {
-////        File configXmlFile = new File("C:\\Users\\Michal\\Desktop\\Config2.xml");
-////        DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
-////
-////        DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
-////        Document doc = dBuilder.parse(configXmlFile);
-////
-////        if (doc != null) {
-////            doc.getDocumentElement().normalize();
-////        }
-////        assert doc != null;
-////        return doc.getElementsByTagName(keyName).item(0).getTextContent();
-////
-////    }
-//}
+import org.w3c.dom.Document;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import java.io.File;
 
+public class ReadXML {
+    //This Class is for obtaining the browser type from an XML
+    public static String getData (String keyName) throws Exception {
+        File configXmlFile = new File("C:\\Users\\Michal\\Desktop\\Config2.xml");
+        DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+        Document doc = dBuilder.parse(configXmlFile);
+        doc.getDocumentElement().normalize();
+        return doc.getElementsByTagName(keyName).item(0).getTextContent();
+    }
+
+}
